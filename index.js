11453a19db69949628c09f081e61b60202634e85
LakshmanKumarMajji123 <majjilakshman5@gmail.com>
2024-07-05 09:15:09 +0530

    added 3 files index.html, index.css, index.js in js_basics folder

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Indai won the T20 world cup 2024");
+
+let description = document.querySelectorAll('h3');
+
+description.forEach((element) => console.log(element));
+
+console.log(description);
+
