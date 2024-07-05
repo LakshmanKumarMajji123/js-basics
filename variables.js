08b4e7bf3985c3aa91b3a99f0532bc78f2de15e1
LakshmanKumarMajji123 <majjilakshman5@gmail.com>
2024-07-05 18:38:24 +0530

    created array.js, datatypes.js, index.js modified some content in those files

---
+++ variables.js
@@ -0,0 +1,30 @@
+//"let" variable 
+
+/**let means simpley "later". later you can easily assign another value to that same "variable"  */
+let a = "lakshmankumar";
+
+console.log(a);
+
+
+a = "ram kumar";
+
+console.log(a);
+
+/**"const" means you cannot chnage the value once you assigned it */
+
+const preparation = "yes iam on the track";
+
+console.log(preparation);
+
+// preparation = "be focus ignore the distraction";
+// console.log(preparation); // it throws an Error
+
+/**before using "let and const" var uses lot's of time */
+
+var players = ["Rohit sharma", "virat kohli", "kuldeep yadav", "hardik pandya"];
+
+ 
+ console.log(players);
+
+
+ console.log("\n\n\n");
